2c3392bcded9b02fab8b3f0d9f60a83277d88df0
Stefan Klinkert <s.klinkert@gmail.com>
2024-06-01 18:57:36 -0400

    cats lab done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    return [...cats, "Broom"];
+}
+
+function prependCat() {
+    return ["Arnold", ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0,2);
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
